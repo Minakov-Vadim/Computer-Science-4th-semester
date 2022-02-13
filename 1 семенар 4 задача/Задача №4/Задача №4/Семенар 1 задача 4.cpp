15a3f81b1181ef470e22e960a5d6e213b0f5cbcc
Minakov-Vadim <minakof.vadim@yandex.ru>
2022-02-14 01:56:23 +0300

    Готовая 4-я задача

--- 1 семенар 4 задача/Задача №4/Задача №4/Семенар 1 задача 4.cpp
+++ 1 семенар 4 задача/Задача №4/Задача №4/Семенар 1 задача 4.cpp
@@ -6,25 +6,85 @@ class Timer
 public:
 	using clock_t = std::chrono::steady_clock;
 	using time_point_t = clock_t::time_point;
-	Timer() : m_begin(clock_t::now()) {}
+	Timer() : m_begin(clock_t::now()), m_flag(false), m_duration(0){}
 	~Timer() noexcept 
 	{
-		std::cout << std::chrono::duration_cast <std::chrono::microseconds> ((clock_t::now()) - m_begin).count() << " microseconds" << std::endl;
-	}
-private:
-	time_point_t m_begin;
-};
-
-int main(int argc, char** argv) {
-	{
-		Timer t;
-		int a = 0;
-		for (int i = 0; i < 10000; i++) {
-			a += i;
+		try
+		{
+			pause();
+		}
+		catch (...)
+		{
+			std::cerr << "You have a mistake" << std::endl;
 		}
 	}
 
+	void pause()
+	{
+		if (m_flag)
+		{
+			m_duration += std::chrono::duration_cast<std::chrono::microseconds>(clock_t::now() - m_begin);
+			m_flag = false;
+		}
+	}
+	void start()
+	{
+		if (m_flag)
+		{
 
-	// На счет перестановок, я доделаю задачу. До понедельника.
+		}
+		else
+		{
+			m_flag = true;
+			m_begin = clock_t::now();
+		}
+	}
+	void finish()
+	{
+		m_duration += std::chrono::duration_cast<std::chrono::microseconds>(clock_t::now() - m_begin);
+		std::cout << m_duration.count() << " microseconds" << std::endl;
+		m_duration -= m_duration;
+		m_flag = false;
+	}
+
+	auto& getter() const noexcept
+	{
+		return m_duration;
+	}
+
+private:
+	time_point_t m_begin;
+	std::chrono::microseconds m_duration;
+	bool m_flag;
+};
+
+int main(int argc, char** argv) {
+	int a = 0;
+	{
+		Timer t;
+
+		t.start();
+		for (int i = 0; i < 10000; i++) {
+			a += i;
+		}
+		t.pause();
+
+
+		for (int i = 0; i < 10000; i++) {
+			a += i;
+		}
+		a = 0;
+
+
+		t.start();
+		for (int i = 0; i < 10000; i++) {
+			a += i;
+		}
+		t.finish();
+
+
+	}
+
+
 	return EXIT_SUCCESS;
 }
