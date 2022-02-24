eecd6544cbe686671e3cf699ab69125cfc2ff88f
Minakov-Vadim <minakof.vadim@yandex.ru>
2022-02-25 01:51:08 +0300

    Исправил задачу №1 2-го семенара

--- 1 семенар 4 задача/Задача №4/Задача №4/Semenar 2 task 1.cpp
+++ 1 семенар 4 задача/Задача №4/Задача №4/Semenar 2 task 1.cpp
@@ -19,14 +19,27 @@ int main(){
 	
 	/*
 	a.reserve(10);
-	for (int i = 0; i < 30; i++) {
+	for (int i = 0; i < 50; i++) {
 		std::cout << a.size() << " " << a.capacity() << '\n';
 		a.push_back(i);
 	}
 	*/
 	//We started multiplying the allocated memory at the beginning by the same factor.
 
-	a.reserve(10000000000000000000);
+
+
+	//The coefficient by which the array size is multiplied is 1.5.
+
+
+	try 
+	{
+		a.reserve(10000000000000000000);
+	}
+	catch(std::exception& exception)
+	{
+		std::cerr << "Standard exception: " << exception.what() << '\n';
+	}
+	
 	// You will get an error std::length_error
     //length_error: исключение, которое возникает при попытке создать объект большего размера, чем допустим для данного типа
 
