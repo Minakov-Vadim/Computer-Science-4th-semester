e3964d7d018d0078c3896c6d30400a80e4f20db5
Minakov-Vadim <minakof.vadim@yandex.ru>
2022-02-25 01:51:38 +0300

    Исправил задачу №2 2-го семенара

--- 1 семенар 4 задача/Задача №4/Задача №4/Semenar 2 task 2.cpp
+++ 1 семенар 4 задача/Задача №4/Задача №4/Semenar 2 task 2.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <chrono>
-
 #include <iterator> 
 #include <algorithm>
-
 #include <array>
 #include <vector>
 #include <deque>
@@ -16,7 +14,7 @@ class Timer
 public:
 	using clock_t = std::chrono::steady_clock;
 	using time_point_t = clock_t::time_point;
-	Timer() : m_begin(clock_t::now()), m_flag(false), m_duration(0) {}
+	Timer() : m_begin(clock_t::now()), m_flag(false), m_doub(0.0) {}
 	~Timer() noexcept
 	{
 		try
@@ -28,12 +26,11 @@ public:
 			std::cerr << "You have a mistake" << std::endl;
 		}
 	}
-
 	void pause()
 	{
 		if (m_flag)
 		{
-			m_duration += std::chrono::duration_cast<std::chrono::microseconds>(clock_t::now() - m_begin);
+			m_doub = std::chrono::duration_cast<std::chrono::microseconds>(clock_t::now() - m_begin).count();
 			m_flag = false;
 		}
 	}
@@ -45,96 +42,84 @@ public:
 		}
 		else
 		{
+			m_doub = 0.0;
 			m_flag = true;
 			m_begin = clock_t::now();
 		}
 	}
-	void finish()
-	{
-		m_duration += std::chrono::duration_cast<std::chrono::microseconds>(clock_t::now() - m_begin);
-		std::cout << m_duration.count() << " microseconds" << std::endl;
-		m_duration -= m_duration;
-		m_flag = false;
-	}
 
-	auto& getter() const noexcept
+	double getter_doub() const noexcept
 	{
-		return m_duration;
+		return m_doub;
 	}
 
 private:
 	time_point_t m_begin;
-	std::chrono::microseconds m_duration;
 	bool m_flag;
+	double m_doub;
 };
 
 
 int main(int argc, char** argv) {
+	
+	const int n = 200000;
+	const int m = 100;
+	double b_1 = 0.0, b_2 = 0.0, b_3 = 0.0, b_4 = 0.0, b_5 = 0.0;
+	std::array <int, n > a_1;
 
-
-	std::array <int, 10000 > a_1;
-	std::vector <int> a_2;
-	std::deque<int> a_3;
-	std::list<int> a_4;
-	std::forward_list<int> a_5;
-
-	int b;
-	std::array <int, 100000 > a;
-	for (int i = 0; i < 10000; ++i) {
-		std::cin >> b;
-		a[i] = b;
-	}
-
-
-	for (int i = 0; i < 10000; ++i) {
-		a_1[i] = a[i];
-		a_2.push_back(a[i]);
-		a_3.push_back(a[i]);
-		a_4.push_back(a[i]);
-		a_5.push_front(a[9999-i]);
-	}
-
-	{
-		Timer t;
-		t.start();
-		std::sort(a_1.begin(), a_1.end());
-		t.finish();
-
-		t.start();
-		std::sort(a_2.begin(), a_2.end());
-		t.finish();
-
-		t.start();
-		std::sort(a_3.begin(), a_3.end());
-		t.finish();
-
-		t.start();
-		a_4.sort();
-		t.finish();
-
-		t.start();
-		a_5.sort();
-		t.finish();
+	for (int j = 0; j < m; ++j) {
 		
+		for (int i = 0; i < n; ++i) {
+			a_1[i] = rand();
+		}
+		std::array<int, n>::const_iterator begin = a_1.begin();
+		std::array<int, n>::const_iterator end = a_1.end();
+
+		std::vector <int> a_2(begin, end);
+		std::deque<int> a_3(begin, end);
+		std::list<int> a_4(begin, end);
+		std::forward_list<int> a_5(begin, end);
+
+
+
+		{
+			Timer t;
+			t.start();
+			std::sort(a_1.begin(), a_1.end());
+			t.pause();
+			b_1 += t.getter_doub();
+
+
+			t.start();
+			std::sort(a_2.begin(), a_2.end());
+			t.pause();
+			b_2 += t.getter_doub();
+
+			t.start();
+			std::sort(a_3.begin(), a_3.end());
+			t.pause();
+			b_3 += t.getter_doub();
+
+			t.start();
+			a_4.sort();
+			t.pause();
+			b_4 += t.getter_doub();
+
+			t.start();
+			a_5.sort();
+			t.pause();
+			b_5 += t.getter_doub();
+
+		}
 	}
 
 
-	//18 15 10 microseconds
-	//31 23 9 microseconds
-	//144 95 122 microseconds
-	//989 565 325 microseconds
-	//515 513 422 microseconds
-	
-
-	
-	///Our winners
-	//1)array
-	//2)vector
-	//3)deque
-	//4)forward_list
-	//5)list
-
-	//Maybe I didn't judge honestly
+	std::cout << "Average working time array = " << b_1 / m << '\n';
+	std::cout << "Average working time vector = " << b_2 / m << '\n';
+	std::cout << "Average working time deque = " << b_3 / m << '\n';
+	std::cout << "Average working time list = " << b_4 / m << '\n';
+	std::cout << "Average working time forward_list = " << b_5 / m << '\n';
+	//Winner array
 
 	return EXIT_SUCCESS;
 }
