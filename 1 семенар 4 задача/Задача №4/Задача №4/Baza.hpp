9dcd6b4332bb4097b82d94ce941803a44d4b204d
Вадим <81166661+Minakov-Vadim@users.noreply.github.com>
2022-05-04 19:10:58 +0300

    Update Baza.hpp

--- 1 семенар 4 задача/Задача №4/Задача №4/Baza.hpp
+++ 1 семенар 4 задача/Задача №4/Задача №4/Baza.hpp
@@ -1,29 +1,38 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include <string>
 #include "Animation.hpp"
+#include <string>
+
+enum Entity
+{
+    Asteroid,
+    Bullet,
+    Explosion,
+    Player
+};
+
 class Baza
 {
 public:
-    static bool collide(const Baza& a, const Baza& b)
+    static bool collide(const Baza& a, const Baza& b) const
     {
         return (b.m_x - a.m_x) * (b.m_x - a.m_x) + (b.m_y - a.m_y) * (b.m_y - a.m_y) <
             (a.m_radius + b.m_radius) * (a.m_radius + b.m_radius);
     }
 
-    Baza(
-        std::string name,
+    explicit Baza(
+        Entity name,
         Animation& anim, float x, float y, float angle = 0.0f,
         float radius = 1.0f) :
         m_anim(anim), m_x(x), m_y(y), m_angle(angle), m_radius(radius),
-        m_name(std::move(name)) {}
+        m_name(name) {}
 
-    virtual bool is_alive() const
+    virtual bool is_alive() const noexcept
     {
         return m_is_alive;
     }
 
-    void die()
+    void die() noexcept
     {
         m_is_alive = false;
     }
@@ -41,21 +50,22 @@ public:
 
     void draw(sf::RenderWindow& app)
     {
-        m_anim.sprite().setPosition(m_x, m_y);
-        m_anim.sprite().setRotation(m_angle + 90);
-        app.draw(m_anim.sprite());
+        auto anim_sprite = m_anim.sprite();
+        anim_sprite.setPosition(m_x, m_y);
+        anim_sprite.setRotation(m_angle + 90);
+        app.draw(anim_sprite);
     }
 
     virtual ~Baza() = default;
 
 public:
-    float x() const { return m_x; }
-    float y() const { return m_y; }
-    float angle() const { return m_angle; }
-    float radius() const { return m_radius; }
-    const std::string& name() const { return m_name; }
+    const float x() const noexcept { return m_x; }
+    const float y() const { return m_y; }
+    const float radius() const noexcept { return m_radius; }
+    const float angle() const noexcept { return m_angle; }
+    const Entity& name() const noexcept { return m_name; }
 
-    const Animation& anim() const { return m_anim; }
+    const Animation& anim() const noexcept { return m_anim; }
     Animation& anim() { return m_anim; }
 
 protected:
@@ -73,7 +83,7 @@ protected:
         if (m_y < 0) { m_y = static_cast<float>(m_anim.H); }
     }
 
-    bool is_out_of_bounds() const
+    const bool is_out_of_bounds() const
     {
         return m_x > m_anim.W || m_x < 0 || m_y > m_anim.H || m_y < 0;
     }
@@ -87,10 +97,11 @@ protected:
     float m_angle;
 
     bool m_is_alive = true;
-    std::string m_name;
 
     float m_dx = 0.0;
     float m_dy = 0.0;
+
+    Entity m_name;
 private:
     virtual void update_impl() = 0;
 };
