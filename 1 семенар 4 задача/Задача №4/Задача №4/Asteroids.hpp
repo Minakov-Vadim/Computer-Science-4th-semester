072a62896ca29872ce8259ef113eed0674a1ea05
Вадим <81166661+Minakov-Vadim@users.noreply.github.com>
2022-05-04 19:11:29 +0300

    Update Asteroids.hpp

--- 1 семенар 4 задача/Задача №4/Задача №4/Asteroids.hpp
+++ 1 семенар 4 задача/Задача №4/Задача №4/Asteroids.hpp
@@ -2,22 +2,25 @@
 #include <SFML/Graphics.hpp>
 #include <random>
 #include "Animation.hpp"
-#include <ctime>
 #include "Baza.hpp"
+#include <ctime>
+
+const int size = 15;//using in Game.hpp
+
 class Asteroids : public Baza
 {
 public:
-    Asteroids(
+    explicit Asteroids(
         Animation& anim, float x, float y, float angle = 0.0f,
-        float radius = 1.0f) : Baza("asteroid", anim, x, y, angle, radius)
-    {
-        std::random_device dev;
-        std::default_random_engine engine(dev());
-        m_dx = static_cast<float>(std::uniform_int_distribution<int>(-4, 4)(engine));
-        m_dy = static_cast<float>(std::uniform_int_distribution<int>(-4, 4)(engine));
-    }
+        float radius = size * 1.0f) : Baza(Entity::Asteroid, anim, x, y, angle, radius),
+        m_dx(m_distribution(m_engine)), m_dy(m_distribution(m_engine))
+    {}
 
 private:
+    static inline std::random_device m_dev;
+    static inline std::default_random_engine m_engine = std::default_random_engine(m_dev());
+    static inline std::uniform_real_distribution<float>(-4, 4) m_distribution;
+
     void update_impl() override
     {
         move();
